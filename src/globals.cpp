0cf2eb2ceac4b0b8e2a02ece8ebe698d42d6db20
Kyosant <foursquarerc@gmail.com>
2023-12-20 15:48:41 -0700

    balls

--- src/globals.cpp
+++ src/globals.cpp
@@ -11,12 +11,12 @@
 
 
 // drivetrain motors
-pros::Motor flm(1, pros::E_MOTOR_GEARSET_06, false); 
-pros::Motor blbm(2, pros::E_MOTOR_GEARSET_06, false); 
-pros::Motor bltm(3, pros::E_MOTOR_GEARSET_06, true); 
-pros::Motor frm(4, pros::E_MOTOR_GEARSET_06, true); 
-pros::Motor brbm(5, pros::E_MOTOR_GEARSET_06, true); 
-pros::Motor brtm(6, pros::E_MOTOR_GEARSET_06, false); 
+pros::Motor flm(11, pros::E_MOTOR_GEARSET_06, false); 
+pros::Motor blbm(12, pros::E_MOTOR_GEARSET_06, false); 
+pros::Motor bltm(13, pros::E_MOTOR_GEARSET_06, true); 
+pros::Motor frm(18, pros::E_MOTOR_GEARSET_06, true); 
+pros::Motor brbm(19, pros::E_MOTOR_GEARSET_06, true); 
+pros::Motor brtm(20, pros::E_MOTOR_GEARSET_06, false); 
 
 // dt motors for constructor
 pros::MotorGroup leftMotors({flm, blbm,bltm});
@@ -26,7 +26,7 @@ pros::MotorGroup rightMotors({frm, brbm, brtm});
 
 
 // other motors
-pros::Motor cataMotor(20, pros::E_MOTOR_GEARSET_18, false);
+pros::Motor cataMotor(16, pros::E_MOTOR_GEARSET_18, false);
 pros::Motor intakeMotor(14, pros::E_MOTOR_GEARSET_06, true);
 
 //other sensors
@@ -37,7 +37,7 @@ pros::ADIDigitalIn lim('D');
 // dt struct
 lemlib::Drivetrain drivetrain(&leftMotors, // left motor group
                               &rightMotors, // right motor group
-                              10, // 10 inch track width
+                              10.3125, // 10 inch track width
                               lemlib::Omniwheel::NEW_275, // using new 3.25" omnis
                               450, // drivetrain rpm is 360
                               2 // chase power is 2. If we had traction wheels, it would have been 8
